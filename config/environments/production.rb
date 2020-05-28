19949f544e798758e885d9b906c2b03d2e3265d2
Aditya Dutta <s3733790@student.rmit.edu.au>
2020-05-28 05:55:40 +0000

    Trying fix for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,11 +94,12 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   host = 'radtuts.herokuapp.com'
-  config.action_mailer.default_url_options = {host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port=>'587',
+    :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
